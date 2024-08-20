89d49a543e70279ae3c8e8c9a7d72a384d0aaf94
uosfz <uosfz@student.kit.edu>
2024-08-20 17:06:43 +0200

    progress bar

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,14 @@
+use std::io::{self, Write};
+
 fn main() {
     let image_width = 256;
     let image_height = 256;
-    print!("P3\n{} {}\n255\n", image_width, image_height);
+    println!("P3\n{} {}\n255", image_width, image_height);
 
     for j in 0..image_height {
+        eprint!("\rScanlines remaining: {}", image_height - j);
+        io::stderr().flush().unwrap();
+
         for i in 0..image_width {
             let r = i as f64 / (image_width - 1) as f64;
             let g = j as f64 / (image_height - 1) as f64;
@@ -15,4 +20,5 @@ fn main() {
             println!("{} {} {}", ir, ig, ib);
         }
     }
+    eprintln!("\rDone.                                ");
 }
